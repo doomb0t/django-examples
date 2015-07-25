0de25aa3cb46617c30a10c8f49d68d0e64eb39bf
JonSonesen <jonathon.sonesen@pcc.edu>
2015-07-24 18:53:47 -0700

    set up submission

--- djangoexample/src/contact/views.py
+++ djangoexample/src/contact/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from django.conf import settings
+
+from django.core.mail import send_mail 
 
 from .forms import contactForm
+
 # Create your views here.
 def contact(request):
+    title = 'Contact'
     form = contactForm(request.POST or None)
     if form.is_valid():
-        print form.cleaned_data['email']
+        comment = form.cleaned_data['comment']
+        name = form.cleaned_data['name']
+        subject = 'Message from MYSITE.com'
+        message = '%s %s' %(comment, name)
+        emailFrom = form.cleaned_data['email']
+        emailTo = [settings.EMAIL_HOST_USER] 
+        #send_mail(subject, message, emailFrom, emailTo, fail_silently=True)
+        title = "Thanks"
+        confirm_message = "Thanks for the message!"
+        form = None
     
-    context = locals()
+    context = { 'title' : title, 'form':form, 'confirm message': confirm_message} 
     template = 'contact.html'
     return render(request, template, context)
--- djangoexample/src/djangopaymentexample/settings.py
+++ djangoexample/src/djangopaymentexample/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+EMAIL_HOST = 'smtp.gmail.cpm'
+EMAIL_HOST_USER = 'iwizardpdx@gmail.com'
+EMAIL_HOST_PASSWORD = 'SUPER SECRET'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
