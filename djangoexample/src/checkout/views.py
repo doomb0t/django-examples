0fd2722b53a94abc70329b202ebb3d57367d4119
JonSonesen <jonathon.sonesen@pcc.edu>
2015-07-29 23:14:40 -0700

    stripe test works

--- djangoexample/src/checkout/views.py
+++ djangoexample/src/checkout/views.py
@@ -1,10 +1,30 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import stripe
 
+stripe.api_key = settings.STRIPE_SECRET_KEY
 # Create your views here.
 @login_required
 def checkout(request):
-    context = {} 
+    publishkey = settings.STRIPE_PUBLISHABLE_KEY
+    if request.method == 'POST':
+        # Get the credit card details submitted by the form        
+        token=request.POST['stripeToken']   
+        print(token)
+        # Create the charge on Stripe's servers 
+        #this will charge the user's card
+        try:
+            charge = stripe.Charge.create(
+                    amount=1000, # amount in cents, again
+                    currency="usd",
+                    source=token,
+                    description="Example charge"
+                    )
+        except stripe.error.CardError, e:
+            # The card has been declined
+                pass
+    context = {'publishkey':publishkey} 
     template = 'checkout.html'
     return render(request, template, context)
 
