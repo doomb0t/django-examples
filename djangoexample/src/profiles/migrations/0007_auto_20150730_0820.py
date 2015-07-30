66d57fe1fa314f683603b0b51b88601025310461
JonSonesen <jonathon.sonesen@pcc.edu>
2015-07-30 01:21:17 -0700

    adds strip_user

---
+++ djangoexample/src/profiles/migrations/0007_auto_20150730_0820.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('profiles', '0006_auto_20150707_0125'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='userStripe',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('stripe_id', models.CharField(max_length=200, null=True, blank=True)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='profile',
+            name='user',
+            field=models.OneToOneField(null=True, blank=True, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
--- djangoexample/src/profiles/models.py
+++ djangoexample/src/profiles/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+from django.conf import settings
+from allauth.account.signals import user_logged_in, user_signed_up
 
 # Create your models here.
 class profile(models.Model):
     name = models.CharField(max_length = 1200)
-    
     description = models.TextField(default='description default')
-    
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, blank=True) 
     location = models.CharField(max_length = 1200,default='My Location', blank=True)
-    
     job = models.CharField(max_length = 1200,null=True)
-        
+    
     def __unicode__(self):
         return self.name
+
+class userStripe(models.Model):
+    stripe_id=models.CharField(max_length=200, null=True, blank=True)
+    def __unicode__(self):
+        if self.stripe_id:
+            return str(self.stripe_id)
+        else:
+            return self.user.username
