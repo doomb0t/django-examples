7a6d6c8d912f42eb0ef6b6c0754cf74e5ca97d2b
JonSonesen <jonathon.sonesen@pcc.edu>
2015-07-30 16:52:20 -0700

    final commit

--- djangoexample/src/checkout/views.py
+++ djangoexample/src/checkout/views.py
@@ -7,18 +7,21 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 # Create your views here.
 @login_required
 def checkout(request):
+    customer_id = request.user.userstripe.stripe_id 
     publishkey = settings.STRIPE_PUBLISHABLE_KEY
     if request.method == 'POST':
         # Get the credit card details submitted by the form        
         token=request.POST['stripeToken']   
-        print(token)
+        
         # Create the charge on Stripe's servers 
         #this will charge the user's card
         try:
+            customer = stripe.Customer.retrieve(customer_id)
+            customer.sources.create(card=token)
             charge = stripe.Charge.create(
                     amount=1000, # amount in cents, again
                     currency="usd",
-                    source=token,
+                    customer=customer,
                     description="Example charge"
                     )
         except stripe.error.CardError, e:
--- djangoexample/src/djangopaymentexample/settings.py
+++ djangoexample/src/djangopaymentexample/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-EMAIL_HOST = 'smtp.gmail.cpm'
-EMAIL_HOST_USER = 'iwizardpdx@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = 'SUPER SECRET'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
@@ -22,7 +22,7 @@ EMAIL_USE_TLS = True
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!bjs9#*dew=&srz8p%3_6u!c!+vg1z$hik^ci+sez#+g&03h*y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
